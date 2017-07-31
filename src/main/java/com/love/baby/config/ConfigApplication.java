f98ef5d9fde84b17d37018af30366c5e1013025e
liangbc <bicong.liang@51zouchuqu.com>
2017-07-31 21:41:03 +0800

    初始化

--- src/main/java/com/love/baby/config/ConfigApplication.java
+++ src/main/java/com/love/baby/config/ConfigApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * Created by liangbc on 2017/7/19.
  */
 
-@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableConfigServer
