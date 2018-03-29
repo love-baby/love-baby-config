55744a89d001f9d8c121d6a5d6d2d1d4651d4432
liangbc <bicong.liang@51zouchuqu.com>
2018-03-29 15:20:08 +0800

    添加身份认证

--- src/main/java/com/love/baby/config/ConfigApplication.java
+++ src/main/java/com/love/baby/config/ConfigApplication.java
@@ -1,13 +1,12 @@
 package com.love.baby.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableAutoConfiguration
-@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
