42264e9552e6e21b02ca2e83ec6eb0f5e7f34caa
liangbc <bicong.liang@51zouchuqu.com>
2018-06-07 11:14:10 +0800

    修改配置文件

--- src/main/java/com/love/baby/config/ConfigApplication.java
+++ src/main/java/com/love/baby/config/ConfigApplication.java
@@ -2,13 +2,13 @@ package com.love.baby.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 23770
  */
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
